b5ecc5f9a846a3e7c8dd63f5f0b95138e194cb16
GoduhinRoman <superchewits2000@gmail.com>
2021-02-11 22:52:35 +0300

    feat: add Feed component

--- my-app/src/pages/GlobalFeed/GlobalFeed.jsx
+++ my-app/src/pages/GlobalFeed/GlobalFeed.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './GlobalFeed.module.scss';
+import useFetch from '../../hooks/useFetch';
+import Feed from '../../components/Feed';
 
 function GlobalFeed() {
+  const [{ response, error, isLoading }, doFetch] = useFetch('/articles?limit=10&offset=0');
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
   return (
-    <div className={style.wrapper}>Global Feed</div>
+    <div className={style.wrapper}>
+      <div className={style.articles}>
+        {isLoading && <div>Loading</div>}
+        {error && <div>{`Error: ${error}`}</div>}
+        {!isLoading && response && <Feed articles={response.articles} />}
+      </div>
+
+      <div className={style.tags}>
+        Tags
+      </div>
+    </div>
   );
 }
 

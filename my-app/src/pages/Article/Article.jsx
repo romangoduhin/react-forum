9e8d016c8ffa9b373ff8a457f876219d40f518d4
GoduhinRoman <superchewits2000@gmail.com>
2021-02-17 02:37:13 +0300

    feat: add Delete and Edit button

--- my-app/src/pages/Article/Article.jsx
+++ my-app/src/pages/Article/Article.jsx
@@ -1,35 +1,98 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
 import style from './Article.module.scss';
 import useFetch from '../../hooks/useFetch';
 import Loading from '../../components/Loading';
 import ErrorMessages from '../../components/ErrorMessages';
 import Author from '../../components/Author';
 import TagsList from '../../components/TagsList';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
 function Article({ match }) {
   const { articlePath } = match.params;
   const apiUrl = `/articles/${articlePath}`;
-  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl);
+  const [{
+    response: fetchArticleResponse,
+    error: fetchArticleError,
+    isLoading: fetchArticleLoading,
+  }, doFetchArticle] = useFetch(apiUrl);
+  const [{
+    response: deleteArticleResponse,
+    isLoading: deleteArticleLoading,
+  }, doDeleteArticle] = useFetch(apiUrl);
+  const [isDeleteSuccess, setIsDeleteSuccess] = useState(false);
+  const [currentUser] = useContext(CurrentUserContext);
+
+  const isAuthor = () => {
+    if (!fetchArticleResponse || !currentUser.isLogged) {
+      return false;
+    }
+    return fetchArticleResponse.article.author.username === currentUser.currentUser.username;
+  };
+
+  const handleDeleteArticle = () => {
+    doDeleteArticle({
+      method: 'delete',
+    });
+  };
 
   useEffect(() => {
-    doFetch();
-  }, [doFetch]);
+    doFetchArticle();
+  }, [doFetchArticle]);
+
+  useEffect(() => {
+    if (!deleteArticleResponse) {
+      return;
+    }
+    setIsDeleteSuccess(true);
+  }, [deleteArticleResponse]);
+
+  if (isDeleteSuccess) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className={style.articleWrapper}>
-      {isLoading && <Loading /> }
-      {error && <ErrorMessages error={error} />}
-      {!isLoading && response && (
+      {fetchArticleLoading && <Loading />}
+      {fetchArticleError && <ErrorMessages error={fetchArticleError} />}
+      {!fetchArticleLoading && fetchArticleResponse && (
         <>
           <div className={style.articleBanner}>
-            <h1>{response.article.title}</h1>
-            <Author author={response.article.author} createdAt={response.article.createdAt} />
+            <div className={style.mainInfoBlock}>
+              <h1>{fetchArticleResponse.article.title}</h1>
+              <Author
+                author={fetchArticleResponse.article.author}
+                createdAt={fetchArticleResponse.article.createdAt}
+              />
+            </div>
+            <div className={style.buttonsBlock}>
+              {isAuthor() && (
+                <>
+                  <Link className={style.editButton} to={`/article/${fetchArticleResponse.article.slug}/edit`}>
+                    <i className="ion-edit" />
+                    Edit article
+                  </Link>
+
+                  <button
+                    className={style.deleteButton}
+                    type="button"
+                    disabled={deleteArticleLoading}
+                    onClick={handleDeleteArticle}
+                  >
+                    <i className="ion-trash-a" />
+                    Delete Article
+                  </button>
+                </>
+              )}
+
+            </div>
           </div>
 
           <div className={style.articleBody}>
-            <p className={style.articleText}>{response.article.body}</p>
+            <p className={style.articleText}>{fetchArticleResponse.article.body}</p>
 
-            <TagsList tagsList={response.article.tagList} />
+            <TagsList tagsList={fetchArticleResponse.article.tagList} />
           </div>
         </>
       )}

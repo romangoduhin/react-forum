80909565e4485e61b30ecb556948a594fdea62a0
GoduhinRoman <superchewits2000@gmail.com>
2021-03-14 20:44:09 +0300

    feat: add new eslint rule

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'react',
   ],
   rules: {
+    'linebreak-style': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/no-extraneous-dependencies': ['error', { devDependencies: false, optionalDependencies: false, peerDependencies: false }],
     'no-unused-vars': 'off',
